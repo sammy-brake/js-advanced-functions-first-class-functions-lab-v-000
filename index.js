594f6cf1a6537ccb6f8267e87e7dd1a7f2c9e400
Samantha brake <sammy.brake@gmail.com>
2019-05-18 04:36:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ const returnLastTwoDrivers = function(drivers) {
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-createFareMultiplier
+function createFareMultiplier(num) {
+  
+};
